bf2029accca77dfa357f70b28bbdaf5b5c5d879b
Rohan Sharma <118556863+Rohansharma10@users.noreply.github.com>
2024-03-16 19:15:31 -0400

    Add files via upload

---
+++ Utilities.h
@@ -0,0 +1,23 @@
+/***********************************************************************
+//  Name:  Rohan
+// student Id: 170442214
+// Email: rohan2@myseneca.ca
+***********************************************************************/
+
+#ifndef SENECA_UTILITIES_H
+#define SENECA_UTILITIES_H
+#include <iostream>
+namespace seneca {
+	std::string trim(std::string& src);
+	class Utilities {
+		size_t m_widthField{ 1 };
+		static char m_delimiter;
+	public:
+		void setFieldWidth(size_t newWidth);
+		size_t getFieldWidth() const;
+		std::string extractToken(const std::string& str, size_t& next_pos, bool& more);
+		static void setDelimiter(char newDelimiter);
+		static char getDelimiter();
+	};
+}
+#endif  
